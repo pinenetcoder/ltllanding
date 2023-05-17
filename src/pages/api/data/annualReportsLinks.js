ee3b6d630bfc394df6f004809d0bac8463bcd7cd
Marius Dainys <marius.dainys@pinenet.io>
2023-05-17 16:24:39 +0300

    LLP-26- Esg Page

---
+++ src/pages/api/data/annualReportsLinks.js
@@ -0,0 +1,68 @@
+export const annualReportsLinks = [
+   {
+      year: '2018',
+      active: false,
+      uid: 0,
+      links: [
+         {
+            linkName: "2018 ESG ataskaita (pdf)",
+            linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
+         },
+      ]
+   },
+   {
+      year: '2019',
+      active: false,
+      uid: 1,
+      links: [
+         {
+            linkName: "2019 ESG ataskaita (pdf)",
+            linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
+         },
+      ]
+   },
+   {
+      year: '2020',
+      active: false,
+      uid: 2,
+      links: [
+         {
+            linkName: "2020 ESG ataskaita (pdf)",
+            linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
+         },
+      ]
+   },
+   {
+      year: '2021',
+      active: false,
+      uid: 3,
+      links: [
+         {
+            linkName: "2021 ESG ataskaita (pdf)",
+            linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
+         },
+      ]
+   },
+   {
+      year: '2022',
+      active: false,
+      uid: 4,
+      links: [
+         {
+            linkName: "2022 ESG ataskaita (pdf)",
+            linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
+         },
+      ]
+   },
+   {
+      year: '2023',
+      active: true,
+      uid: 5,
+      links: [
+         {
+            linkName: "2023 ESG ataskaita (pdf)",
+            linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
+         },
+      ]
+   },
+]
