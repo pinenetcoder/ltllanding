975bf801556f6a7b5caac00dd742084c4079c064
mdainys <marius.dainys@pinenet.io>
2023-07-04 10:02:44 +0300

    LLP-61- resolving firebase problem

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
